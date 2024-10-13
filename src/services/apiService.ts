53f4f27f47a9d11032f52adb1a7358fecbe45a41
Mark Morozov <morozov-mark@bk.ru>
2024-10-13 10:33:48 +0500

    updated api

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -2,10 +2,14 @@ import axios from "axios";
 
 export const API_BASE_URL = import.meta.env.PROD
   ? "/"
-  : "http://localhost:8080";
+  : "https://api.jsonbin.io/v3/b/670b5913acd3cb34a895f47e";
 // Create an Axios instance
 const apiService = axios.create({
   baseURL: API_BASE_URL,
+  headers: {
+    "X-Master-Key":
+      "$2a$10$NmgrNTotuJ8IsC9Ymec57uFHGgrxutgI.es8fd9aI0Oj6dPD76l5a",
+  },
 });
 
 export default apiService;
